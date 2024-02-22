5609b4a6857dbbeb1a5e42c57fbc4c57ba9a281f
byang12159 <92831530+byang12159@users.noreply.github.com>
2024-02-22 09:12:41 -0600

    “working progress"

--- calibrate_extrin.py
+++ calibrate_extrin.py
@@ -2,17 +2,47 @@ import sys
 import pickle
 import numpy as np
 from scipy.optimize import least_squares
-# from DroneExperiments.vision.cam2drone import get_T_DC
+from picam.transformation_properties_drone import get_T_DC
 from utils import transformations
 
-with open("calibration_data_test2.p",'rb') as handle:
+with open("calibration_data_test4.p",'rb') as handle:
     data = pickle.load(handle)
 
-pose_C = np.array([d[0] for d in data]) # N x 4 x 4
-pose_W = np.array([d[4] for d in data]) # N x 4 x 4
+pose_C = []
+pose_W = []
+T_WD = []
 
-T_WD = np.array([d[1] for d in data]) # N x 4 x 4
+target_ID = 0 
+
+for j in range(len(data)):
+    if len(data[j][2][0]) is not 0:
+        for a in range(len(data[j][2][1])):
+            if data[j][2][1][a] == target_ID:
+
+                snapstate = data[j][1]
+                T_WD_j = transformations.quaternion_matrix(snapstate[11:15])
+                T_WD_j[:3,3] = snapstate[:3]
+                T_WD.append(T_WD_j)
+
+                Ts = data[j][2][0][a]
+                Ts_inv = np.linalg.inv(Ts)
+                pose_C.append(Ts_inv)
+
+                marker_GT = data[0][0]
+                q = marker_GT[11:15] # x, y, z, w
+                T_WM = transformations.quaternion_matrix(q)
+                T_WM[:3, 3] = marker_GT[:3]
+                pose_W.append(T_WM)
+    
+pose_C = np.array(pose_C) # N x 4 x 4
+pose_W = np.array(pose_W) # N x 4 x 4
+T_WD = np.array(T_WD) # N x 4 x 4
+print(pose_C.shape)
+print(pose_W.shape)
+print(T_WD.shape)
+
 marker_gt = np.array(data[0][0][:3]).reshape(1, -1)
+
 idx = np.where(((pose_W[:,:3,3] - marker_gt)**2).sum(axis=1)<0.1)[0]
 print('# raw samples: %d, # filtered samples: %d'%(pose_W.shape[0], len(idx)))
 pose_C = pose_C[idx,:,:]
@@ -37,9 +67,9 @@ x0[4:] = get_T_DC()[:3, 3]
 
 # import ipdb; ipdb.set_trace()
 
-print(loss(x0))
+print(f"Starting loss: {loss(x0)}")
 res = least_squares(loss, x0)
-print(loss(res.x))
+print(f"Ending Loss {loss(res.x)}")
 q=res.x[:4]
 t=res.x[4:]
 euler = transformations.euler_from_quaternion(q, 'syxz')
--- test3.py
+++ test3.py
@@ -4,84 +4,118 @@ from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 from utils import transformations
 
-xaxis_h = np.array([1,0,0,1])
-yaxis_h = np.array([0,1,0,1])
-zaxis_h = np.array([0,0,1,1])
-
 with open("calibration_data_test4.p",'rb') as handle:
     data = pickle.load(handle)
 
 marker_GT = data[0][0]
 idx_spotted = []
+Ts_full = []
+snapstate_full = []
+target_ID = 0
 
 for j in range(len(data)):
     if len(data[j][2][0]) is not 0:
-        idx_spotted.append(j)
+        for a in range(len(data[j][2][1])):
+            if data[j][2][1][a] == target_ID:
+                # print(data[j][2][0][a])
+
+                idx_spotted.append(j)
+                Ts_full.append(data[j][2][0][a])
+                snapstate_full.append(data[j][1])
+
+scale = 0.4
+xaxis_h = np.array([1*scale,0,0,1])
+yaxis_h = np.array([0,1*scale,0,1])
+zaxis_h = np.array([0,0,1*scale,1])
+
+# Unit vectors along axes
+xaxis = np.array([1, 0, 0])
+yaxis = np.array([0, 1, 0])
+zaxis = np.array([0, 0, 1])
 
 q = marker_GT[11:15] # x, y, z, w
 T_WM = transformations.quaternion_matrix(q)
 T_WM[:3, 3] = marker_GT[:3]
 
+fig = plt.figure()
+ax = fig.add_subplot(111,projection='3d')
+ax.plot(0,0,0, 'x',color='red',label="Marker Center")
 scale = 1
+ax.plot([0,xaxis[0]*scale],[0,xaxis[1]*scale],[0,xaxis[2]*scale],color='m')
+ax.plot([0,yaxis[0]*scale],[0,yaxis[1]*scale],[0,yaxis[2]*scale],color='y')
+ax.plot([0,zaxis[0]*scale],[0,zaxis[1]*scale],[0,zaxis[2]*scale],color='c')
+
+for i in range(len(idx_spotted)):
+    Ts = Ts_full[i]
+    snapstate = snapstate_full[i]
+
+    translation = Ts[:3,3]
+    rotation = np.linalg.inv(Ts[:3,:3])
+    Ts_inv = np.linalg.inv(Ts)
+
+    aruco_head =   Ts_inv.dot(np.array([0,0,0,1]))
+    aruco_tail_x = Ts_inv.dot(xaxis_h)
+    aruco_tail_y = Ts_inv.dot(yaxis_h)
+    aruco_tail_z = Ts_inv.dot(zaxis_h)
+
+    ax.plot([aruco_head[0],aruco_tail_x[0]],[aruco_head[1],aruco_tail_x[1]],[aruco_head[2],aruco_tail_x[2]],color='red',linewidth = 0.5)
+    ax.plot([aruco_head[0],aruco_tail_y[0]],[aruco_head[1],aruco_tail_y[1]],[aruco_head[2],aruco_tail_y[2]],color='green',linewidth = 0.5)
+    ax.plot([aruco_head[0],aruco_tail_z[0]],[aruco_head[1],aruco_tail_z[1]],[aruco_head[2],aruco_tail_z[2]],color='blue',linewidth = 0.5)
+
+    extrinsic = [-1.1821123183663413, 0.6909067890182983, -0.3760973929406938, -0.046566694921098876, -0.3191826790883203, 1.54573429744888, 0.5815594116570081]
+    quaternion = extrinsic[:4]
+    translation = extrinsic[4:]
+    T_DC = transformations.quaternion_matrix(quaternion)
+    T_DC[:3, 3] = translation
+    print("T_DC\n",T_DC, "\n euler\n",(180/np.pi)*np.array(transformations.euler_from_quaternion(quaternion)))
+
+
+    T_WD = transformations.quaternion_matrix(snapstate[11:15])
+    T_WD[:3,3] = snapstate[:3]
+
+    # ax.plot(snapstate[0],snapstate[1],snapstate[2],'x',color='green',label='Drone Ground Truth')
+    drone_head = Ts_inv@T_DC@np.array([0,0,0,1])
+    ax.plot(drone_head[0],drone_head[1],drone_head[2],'x',color='purple',label='Drone Ground Truth')
+
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')
+# plt.legend()
+plt.axis('equal')
+plt.show()
+
 
 fig = plt.figure()
 ax = fig.add_subplot(111,projection='3d')
-for i in idx_spotted:
-    Ts = data[i][2][0][0]
-    snapstate = data[i][1]
+ax.plot(0,0,0, 'x',color='red',label="World Center")
+scale = 1
+ax.plot([0,xaxis[0]*scale],[0,xaxis[1]*scale],[0,xaxis[2]*scale],color='m')
+ax.plot([0,yaxis[0]*scale],[0,yaxis[1]*scale],[0,yaxis[2]*scale],color='y')
+ax.plot([0,zaxis[0]*scale],[0,zaxis[1]*scale],[0,zaxis[2]*scale],color='c')
 
-    # Unit vectors along axes
-    xaxis = np.array([1, 0, 0])
-    yaxis = np.array([0, 1, 0])
-    zaxis = np.array([0, 0, 1])
+count = 0
+for i in range(len(idx_spotted)):
+    Ts = Ts_full[i]
+    snapstate = snapstate_full[i]
 
-    q = marker_GT[11:15] # x, y, z, w
-    T_WM = transformations.quaternion_matrix(q)
-    T_WM[:3, 3] = marker_GT[:3]
-
-    scale = 1
-
-    ax.plot(0,0,0, 'x',color='red',label="Vicon Zero")
     ax.plot(snapstate[0],snapstate[1],snapstate[2],'x',color='green',label='Drone Ground Truth')
-    # ax.plot(marker_GT[0],marker_GT[1],marker_GT[2],'x',color='cyan',label='Marker Ground Truth')
+
     ########### PLOT for Marker Poses ###########
-    xaxis_h = np.array([1,0,0,1])
-    yaxis_h = np.array([0,1,0,1])
-    zaxis_h = np.array([0,0,1,1])
+    scale = 0.5
+    xaxis_h = np.array([1*scale,0,0,1])
+    yaxis_h = np.array([0,1*scale,0,1])
+    zaxis_h = np.array([0,0,1*scale,1])
 
     marker_center = T_WM[:4,3]
     marker_tail_x = T_WM@xaxis_h
     marker_tail_y = T_WM@yaxis_h
     marker_tail_z = T_WM@zaxis_h
 
-    # ax.plot([marker_center[0],marker_tail_x[0]*scale],[marker_center[1],marker_tail_x[1]*scale],[marker_center[2],marker_tail_x[2]*scale],color='red')
-    # ax.plot([marker_center[0],marker_tail_y[0]*scale],[marker_center[1],marker_tail_y[1]*scale],[marker_center[2],marker_tail_y[2]*scale],color='green')
-    # ax.plot([marker_center[0],marker_tail_z[0]*scale],[marker_center[1],marker_tail_z[1]*scale],[marker_center[2],marker_tail_z[2]*scale],color='blue')
+    ax.plot([marker_center[0],marker_tail_x[0]],[marker_center[1],marker_tail_x[1]],[marker_center[2],marker_tail_x[2]],color='red')
+    ax.plot([marker_center[0],marker_tail_y[0]],[marker_center[1],marker_tail_y[1]],[marker_center[2],marker_tail_y[2]],color='green')
+    ax.plot([marker_center[0],marker_tail_z[0]],[marker_center[1],marker_tail_z[1]],[marker_center[2],marker_tail_z[2]],color='blue')
 
-
-    # ########### PLOT for Camera Poses ###########
-    translation = Ts[:3,3]
-    rotation = np.linalg.inv(Ts[:3,:3])
-    Ts_inv = np.linalg.inv(Ts)
-
-    aruco_head =   T_WM@Ts_inv.dot(np.array([0,0,0,1]))
-    aruco_tail_x = T_WM@Ts_inv.dot(xaxis_h*scale)
-    aruco_tail_y = T_WM@Ts_inv.dot(yaxis_h*scale)
-    aruco_tail_z = T_WM@Ts_inv.dot(zaxis_h*scale)
-    # aruco_head =   Ts@T_WM.dot(np.array([0,0,0,1]))
-    # aruco_tail_x = Ts@T_WM.dot(xaxis_h*scale)
-    # aruco_tail_y = Ts@T_WM.dot(yaxis_h*scale)
-    # aruco_tail_z = Ts@T_WM.dot(zaxis_h*scale)
-
-    # print("one",T_WM@Ts_inv)
-    # print("two",T_WM.dot(Ts_inv))
-
-    # ax.plot([aruco_head[0],aruco_tail_x[0]],[aruco_head[1],aruco_tail_x[1]],[aruco_head[2],aruco_tail_x[2]],color='red')
-    # ax.plot([aruco_head[0],aruco_tail_y[0]],[aruco_head[1],aruco_tail_y[1]],[aruco_head[2],aruco_tail_y[2]],color='green')
-    # ax.plot([aruco_head[0],aruco_tail_z[0]],[aruco_head[1],aruco_tail_z[1]],[aruco_head[2],aruco_tail_z[2]],color='blue')
-
-
-    ########### PLOT for Drone Poses ###########
+    ########## PLOT for Drone Poses ###########
     T_WD = transformations.quaternion_matrix(snapstate[11:15])
     T_WD[:3,3] = snapstate[:3]
     drone_axes_scale = 1
@@ -91,23 +125,131 @@ for i in idx_spotted:
     drone_axes_tip_y = T_WD@yaxis_h
     drone_axes_tip_z = T_WD@zaxis_h
 
-    # # ax.plot(drone_head[0],drone_head[1],drone_head[2],'x',color='green')
-    # ax.plot([drone_head[0],drone_axes_tip_x[0]],[drone_head[1],drone_axes_tip_x[1]],[drone_head[2],drone_axes_tip_x[2]],color='red')
-    # ax.plot([drone_head[0],drone_axes_tip_y[0]],[drone_head[1],drone_axes_tip_y[1]],[drone_head[2],drone_axes_tip_y[2]],color='green')
-    # ax.plot([drone_head[0],drone_axes_tip_z[0]],[drone_head[1],drone_axes_tip_z[1]],[drone_head[2],drone_axes_tip_z[2]],color='blue')
+    # ax.plot(drone_head[0],drone_head[1],drone_head[2],'x',color='green')
+    ax.plot([drone_head[0],drone_axes_tip_x[0]],[drone_head[1],drone_axes_tip_x[1]],[drone_head[2],drone_axes_tip_x[2]],color='red',linewidth=0.5)
+    ax.plot([drone_head[0],drone_axes_tip_y[0]],[drone_head[1],drone_axes_tip_y[1]],[drone_head[2],drone_axes_tip_y[2]],color='green',linewidth=0.5)
+    ax.plot([drone_head[0],drone_axes_tip_z[0]],[drone_head[1],drone_axes_tip_z[1]],[drone_head[2],drone_axes_tip_z[2]],color='blue',linewidth=0.5)
+
+    count += 1
+    if count >= 100:
+        break
+
+    # translation = Ts[:3,3]
+    # rotation = np.linalg.inv(Ts[:3,:3])
+    # Ts_inv = np.linalg.inv(Ts)
+
+    # aruco_head =   Ts_inv.dot(np.array([0,0,0,1]))
+    # aruco_tail_x = Ts_inv.dot(xaxis_h)
+    # aruco_tail_y = Ts_inv.dot(yaxis_h)
+    # aruco_tail_z = Ts_inv.dot(zaxis_h)
+
+    # ax.plot([aruco_head[0],aruco_tail_x[0]],[aruco_head[1],aruco_tail_x[1]],[aruco_head[2],aruco_tail_x[2]],color='red',linewidth = 0.5)
+    # ax.plot([aruco_head[0],aruco_tail_y[0]],[aruco_head[1],aruco_tail_y[1]],[aruco_head[2],aruco_tail_y[2]],color='green',linewidth = 0.5)
+    # ax.plot([aruco_head[0],aruco_tail_z[0]],[aruco_head[1],aruco_tail_z[1]],[aruco_head[2],aruco_tail_z[2]],color='blue',linewidth = 0.5)
+
+    # extrinsic = [-1.1821123183663413, 0.6909067890182983, -0.3760973929406938, -0.046566694921098876, -0.3191826790883203, 1.54573429744888, 0.5815594116570081]
+    # quaternion = extrinsic[:4]
+    # translation = extrinsic[4:]
+    # T_DC = transformations.quaternion_matrix(quaternion)
+    # T_DC[:3, 3] = translation
+
+    # T_WD = transformations.quaternion_matrix(snapstate[11:15])
+    # T_WD[:3,3] = snapstate[:3]
+
+
+    # drone_head = Ts_inv@T_DC@np.array([0,0,0,1])
+    # ax.plot(drone_head[0],drone_head[1],drone_head[2],'x',color='purple',label='Drone Ground Truth')
 
 ax.set_xlabel('x')
 ax.set_ylabel('y')
 ax.set_zlabel('z')
 # plt.legend()
 plt.axis('equal')
-print("marker estimate at position ",aruco_head[0],aruco_head[1],aruco_head[2])
 plt.show()
 
 
-    # print("difference",drone_head-aruco_head)
 
-    # print("marker location \n",marker_center)
-    # print("drone location \n",drone_head)
-    # print("camera location \n",aruco_head)
+
+# fig = plt.figure()
+# ax = fig.add_subplot(111,projection='3d')
+# for i in range(len(idx_spotted)):
+#     Ts = Ts_full[i]
+#     snapstate = snapstate_full[i]
+
+#     q = marker_GT[11:15] # x, y, z, w
+#     T_WM = transformations.quaternion_matrix(q)
+#     T_WM[:3, 3] = marker_GT[:3]
+
+#     scale = 1
+
+#     ax.plot(0,0,0, 'x',color='red',label="Vicon Zero")
+#     ax.plot(snapstate[0],snapstate[1],snapstate[2],'x',color='green',label='Drone Ground Truth')
+#     # ax.plot(marker_GT[0],marker_GT[1],marker_GT[2],'x',color='cyan',label='Marker Ground Truth')
+#     ########### PLOT for Marker Poses ###########
+#     scale = 0.5
+#     xaxis_h = np.array([1*scale,0,0,1])
+#     yaxis_h = np.array([0,1*scale,0,1])
+#     zaxis_h = np.array([0,0,1*scale,1])
+
+#     marker_center = T_WM[:4,3]
+#     marker_tail_x = T_WM@xaxis_h
+#     marker_tail_y = T_WM@yaxis_h
+#     marker_tail_z = T_WM@zaxis_h
+
+#     # ax.plot([marker_center[0],marker_tail_x[0]*scale],[marker_center[1],marker_tail_x[1]*scale],[marker_center[2],marker_tail_x[2]*scale],color='red')
+#     # ax.plot([marker_center[0],marker_tail_y[0]*scale],[marker_center[1],marker_tail_y[1]*scale],[marker_center[2],marker_tail_y[2]*scale],color='green')
+#     # ax.plot([marker_center[0],marker_tail_z[0]*scale],[marker_center[1],marker_tail_z[1]*scale],[marker_center[2],marker_tail_z[2]*scale],color='blue')
+
+
+#     # ########### PLOT for Camera Poses ###########
+#     translation = Ts[:3,3]
+#     rotation = np.linalg.inv(Ts[:3,:3])
+#     Ts_inv = np.linalg.inv(Ts)
+
+#     aruco_head =   T_WM@Ts_inv.dot(np.array([0,0,0,1]))
+#     aruco_tail_x = T_WM@Ts_inv.dot(xaxis_h)
+#     aruco_tail_y = T_WM@Ts_inv.dot(yaxis_h)
+#     aruco_tail_z = T_WM@Ts_inv.dot(zaxis_h)
+#     # aruco_head =   Ts@T_WM.dot(np.array([0,0,0,1]))
+#     # aruco_tail_x = Ts@T_WM.dot(xaxis_h*scale)
+#     # aruco_tail_y = Ts@T_WM.dot(yaxis_h*scale)
+#     # aruco_tail_z = Ts@T_WM.dot(zaxis_h*scale)
+
+#     # print("one",T_WM@Ts_inv)
+#     # print("two",T_WM.dot(Ts_inv))
+
+#     ax.plot([aruco_head[0],aruco_tail_x[0]],[aruco_head[1],aruco_tail_x[1]],[aruco_head[2],aruco_tail_x[2]],color='red',linewidth = 0.5)
+#     ax.plot([aruco_head[0],aruco_tail_y[0]],[aruco_head[1],aruco_tail_y[1]],[aruco_head[2],aruco_tail_y[2]],color='green',linewidth = 0.5)
+#     ax.plot([aruco_head[0],aruco_tail_z[0]],[aruco_head[1],aruco_tail_z[1]],[aruco_head[2],aruco_tail_z[2]],color='blue',linewidth = 0.5)
+
+
+#     ########### PLOT for Drone Poses ###########
+#     T_WD = transformations.quaternion_matrix(snapstate[11:15])
+#     T_WD[:3,3] = snapstate[:3]
+#     drone_axes_scale = 1
+
+#     drone_head = T_WD[:3,3]
+#     drone_axes_tip_x = T_WD@xaxis_h
+#     drone_axes_tip_y = T_WD@yaxis_h
+#     drone_axes_tip_z = T_WD@zaxis_h
+
+#     # # ax.plot(drone_head[0],drone_head[1],drone_head[2],'x',color='green')
+#     # ax.plot([drone_head[0],drone_axes_tip_x[0]],[drone_head[1],drone_axes_tip_x[1]],[drone_head[2],drone_axes_tip_x[2]],color='red')
+#     # ax.plot([drone_head[0],drone_axes_tip_y[0]],[drone_head[1],drone_axes_tip_y[1]],[drone_head[2],drone_axes_tip_y[2]],color='green')
+#     # ax.plot([drone_head[0],drone_axes_tip_z[0]],[drone_head[1],drone_axes_tip_z[1]],[drone_head[2],drone_axes_tip_z[2]],color='blue')
+
+# ax.set_xlabel('x')
+# ax.set_ylabel('y')
+# ax.set_zlabel('z')
+# # plt.legend()
+# plt.axis('equal')
+# print("marker estimate at position ",aruco_head[0],aruco_head[1],aruco_head[2])
+# plt.show()
+
+
+#     # print("difference",drone_head-aruco_head)
+
+#     # print("marker location \n",marker_center)
+#     # print("drone location \n",drone_head)
+#     # print("camera location \n",aruco_head)
     
