78c621c727ec0aad39aab0b8b38f531cc1754814
Ben Yang <byang11259@hotmail.com>
2024-06-11 18:19:32 -0500

    adjusted cycle rate

--- autonomous_ws/src/autonomous_pkg/scripts/deploy_vel.py
+++ autonomous_ws/src/autonomous_pkg/scripts/deploy_vel.py
@@ -16,14 +16,12 @@ import time
 
 class Deploy:
     def __init__(self):
-        self.target_poses = []
+        self.target_pose = None
         self.current_pose = None
         self.pub = None
         self.stop_event = threading.Event() # Create a global event that can be used to signal the loop to stop
         self.position_history = []
         
-        # User Parameters:
-        self.target_queue = 10
 
         # Control Parameters:
         self.K_p = 1.2
@@ -31,7 +29,7 @@ class Deploy:
         self.K_d = 0.0
         self.PID_integral_max = 10
         self.PID_integral_min = -10
-        self.ctrl_dt = 0.01
+        self.ctrl_dt = 0.05
 
         # Debug use:
         self.debug_callback_time = None
@@ -40,11 +38,8 @@ class Deploy:
         self.time_start = time.time()
 
         try:
-            rospy.loginfo("Experiment: {}".format(self.time_start))
-            
             self.ctrl_thread = threading.Thread(target=self.control_scheduler, args=())
             self.ctrl_thread.start()
-
             self.main()
         except rospy.ROSInterruptException:
             rospy.loginfo("ROSInterruptException caught")
@@ -68,6 +63,7 @@ class Deploy:
         rospy.init_node('deploy_node', anonymous=True)
 
         self.configure_logging()
+        rospy.loginfo("Experiment: {}".format(self.time_start))
 
         sub_startpoint = rospy.Subscriber("/kingfisher/agiros_pilot/state", agiros_msgs.QuadState, self.callback_state)
 
@@ -77,6 +73,11 @@ class Deploy:
         # sub_aruco = rospy.Subscriber("/fake_waypoint", geometry_msgs.Point, self.callback_target)
 
         quad_namespace = "kingfisher"
+
+        # self.pub_pos = rospy.Publisher(quad_namespace+"/agiros_pilot/go_to_pose", 
+        #                     geometry_msgs.PoseStamped, 
+        #                     queue_size=1)
+
         self.pub = rospy.Publisher(quad_namespace+"/agiros_pilot/velocity_command", 
                         geometry_msgs.TwistStamped, 
                         queue_size=1)
@@ -114,17 +115,15 @@ class Deploy:
     
     def callback_target(self, data):
         self.debug_callback_time = time.time()
-        self.target_poses.append( np.array([data.x+self.current_pose[0], data.y+self.current_pose[1], data.z+self.current_pose[2]]))
+        self.target_pose = np.array([data.x+self.current_pose[0], data.y+self.current_pose[1], data.z+self.current_pose[2]])
 
-        if len(self.target_poses) >= self.target_queue:
-            self.target_poses.pop(0)
 
     def control_scheduler(self):
         first = True
         while not self.stop_event.is_set():
             # check for new target points
-            if len(self.target_poses) > 0:
-                active_target_pose = np.copy(self.target_poses[-1])
+            if self.target_pose is not None:
+                active_target_pose = np.copy(self.target_pose)
 
                 integral_error = np.array([0.0, 0.0, 0.0])
                 previous_error = np.array([0.0, 0.0, 0.0])
@@ -134,10 +133,11 @@ class Deploy:
 
                 while (not self.stop_event.is_set() 
                     and self.current_pose is not None 
-                    and np.array_equal(active_target_pose, self.target_poses[-1] )
+                    and np.array_equal(active_target_pose, self.target_pose)
                     and not self._reached_target_position(self.current_pose, active_target_pose)):
                     
-                    target_pose = self.target_poses[-1]
+                    #MATBE CHANGE
+                    target_pose = np.copy(self.target_pose)
     
                     self.position_history.append(self.current_pose)
 
@@ -178,12 +178,12 @@ class Deploy:
                 rospy.loginfo("Exiting velPID loop")
 
     def _reached_target_position(self, current_position, target_position):
-        position_tolerance = 0.1
+        position_tolerance = 0.01
         return la.norm(current_position - target_position) < position_tolerance
 
     def _limitVelocity(self, velocity):
         # Only check y now
-        max_velocity = 3.0
+        max_velocity = 4.0
         velocity[1] = np.clip(velocity[1], -max_velocity, max_velocity)
         
         return velocity
