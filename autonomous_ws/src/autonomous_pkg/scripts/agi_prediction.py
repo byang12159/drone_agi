f58f086f67df667d195ed1b8c2a8db7dc076adfd
Ben Yang <bcyang2@illinois.edu>
2024-07-22 08:05:09 -0500

    "corrected agi prediction for new experiments"

--- autonomous_ws/src/autonomous_pkg/scripts/agi_prediction.py
+++ autonomous_ws/src/autonomous_pkg/scripts/agi_prediction.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 import torch
 import time
 
-torch.manual_seed(42)
+# torch.manual_seed(42)
 class Prediction():
-    def __init__(self, fov_h= 70):
+    def __init__(self, num_trajectory, steps, fov_h= 70):
         # Given FOV of pinhole camera and distance from camera, computes the rectangle range of observable image
         # Arducam B0385 Global Shutter Camera
 
@@ -15,6 +15,12 @@ class Prediction():
         self.aspect_ratio = torch.tensor(4/3).to(self.device)
         self.fov_h_radians = self.fov_h*torch.pi/180.0
         self.fov_v_radians = 2 * torch.atan(torch.tan(self.fov_h_radians / 2) / self.aspect_ratio)
+
+        self.num_trajectory = num_trajectory
+        self.steps = steps
+
+        # create steps x6 due to 6D dimension state: x y z vx vy vz
+        self.total_trajectories = torch.empty(self.num_trajectory, (self.steps+1)*6 ).to(self.device)
         
     
     def calculate_viewable_area(self, distance):
@@ -24,8 +30,7 @@ class Prediction():
         return viewable_width, viewable_height
 
     def calculate_backoff(self,bound):
-        return bound/(torch.tan(self.fov_h_radians / 2))
-
+        return (bound/2)/(torch.tan(self.fov_h_radians / 2))
 
     def plot_rec(self, ax, min_x,max_x,min_y,max_y,min_z,max_z):
         x = [min_x, max_x, max_x, min_x, min_x, max_x, max_x, min_x]
@@ -58,126 +63,117 @@ class Prediction():
 
         return min_radius
 
-    def find_prediction(self, initial_state, ego_state, timestep, accel_range, steps = 2, num_trajectory = 100, visualize = False):
+    def compute_reach(self, initial_state,  timestep, accel_range,  detection_rate):
         
         initial_state = torch.from_numpy(initial_state).to(self.device)
-        ego_state = torch.from_numpy(ego_state).to(self.device)
+        # ego_state = torch.from_numpy(ego_state).to(self.device)
 
-        # create x6 due to 6D dimension: x y z vx vy vz
-        total_trajectories = torch.empty(num_trajectory, (steps+1)*6 ).to(self.device)
+        self.total_trajectories[:, 0] = initial_state[0]
+        self.total_trajectories[:, 1] = initial_state[1]
+        self.total_trajectories[:, 2] = initial_state[2]
+        self.total_trajectories[:, 3] = initial_state[3]
+        self.total_trajectories[:, 4] = initial_state[4]
+        self.total_trajectories[:, 5] = initial_state[5]
 
-        total_trajectories[:, 0] = initial_state[0]
-        total_trajectories[:, 1] = initial_state[1]
-        total_trajectories[:, 2] = initial_state[2]
-        total_trajectories[:, 3] = initial_state[3]
-        total_trajectories[:, 4] = initial_state[4]
-        total_trajectories[:, 5] = initial_state[5]
+        for s in range(6, self.total_trajectories.shape[1], 6):
+            sample_accel = torch.rand(self.num_trajectory,3).to(self.device)* (2 * accel_range) - accel_range
+            new_vel = self.total_trajectories[:,s-3:s]   + sample_accel * timestep
+            new_pos = self.total_trajectories[:,s-6:s-3] + new_vel * timestep
+      
+            self.total_trajectories[:, s]   = new_pos[:,0]
+            self.total_trajectories[:, s+1] = new_pos[:,1]
+            self.total_trajectories[:, s+2] = new_pos[:,2]
+            self.total_trajectories[:, s+3] = new_vel[:,0]
+            self.total_trajectories[:, s+4] = new_vel[:,1]
+            self.total_trajectories[:, s+5] = new_vel[:,2]
+  
 
-        for s in range(6, total_trajectories.shape[1], 6):
-            sample_accel = torch.rand(num_trajectory,3).to(self.device)* (2 * accel_range) + accel_range
-            new_vel = total_trajectories[:,s-3:s]   + sample_accel * timestep
-            new_pos = total_trajectories[:,s-6:s-3] + new_vel * timestep
-        
-            # print(sample_accel.shape)
-            # print(new_vel.shape)
-            # print(new_pos.shape)
-            # print(total_trajectories[:, s].shape)
-            # print(torch.rand(num_trajectory).shape)
-            total_trajectories[:, s]   = new_pos[:,0]
-            total_trajectories[:, s+1] = new_pos[:,1]
-            total_trajectories[:, s+2] = new_pos[:,2]
-            total_trajectories[:, s+3] = new_vel[:,0]
-            total_trajectories[:, s+4] = new_vel[:,1]
-            total_trajectories[:, s+5] = new_vel[:,2]
-                
+        # Find bound of volume and impose IRL Flying arena physical constraints
+        x_vals = self.total_trajectories[:,0::6]
+        y_vals = self.total_trajectories[:,1::6]
+        z_vals = self.total_trajectories[:,2::6]
+        min_x = max(torch.min(x_vals).item(), initial_state[0]-0.5).item()
+        max_x = min(torch.max(x_vals).item(), initial_state[0]+0.3).item()
+        min_y = torch.min(y_vals).item()
+        max_y = torch.max(y_vals).item()
+        min_z = torch.min(z_vals).item()
+        max_z = torch.max(z_vals).item()
+        bounds = [min_x,max_x, min_y,max_y, min_z,max_z]
 
-        # Find Hyper-rectangles of Trajectories
-        rectangle = []
+        # Compute needed backoff given y-bound
+        backoff = self.calculate_backoff((max_y-min_y))
 
-        bound_y,bound_z = self.calculate_viewable_area(torch.norm(ego_state-initial_state[:3]))
+        return self.total_trajectories, backoff.item(), bounds
 
-        for s in range(6, total_trajectories.shape[1], 6):
-            min_x = torch.min(total_trajectories[:,s])
-            max_x = torch.max(total_trajectories[:,s])
-            min_y = torch.min(total_trajectories[:,s+1])
-            max_y = torch.max(total_trajectories[:,s+1])
-            min_z = torch.min(total_trajectories[:,s+2])
-            max_z = torch.max(total_trajectories[:,s+2])
+       
 
-            if max_y+initial_state[1] > (bound_y/2)+ego_state[1] or min_y+initial_state[1] > ego_state[1]-(bound_y/2):
-                return self.calculate_backoff(bound_y).item()
-            else:
-                return 0.0
-            # if torch.abs(max_z-min_z)  > bound_z:
-            #     break
-            # rectangle.append([min_x,max_x,min_y,max_y,min_z,max_z])
-
-        # # Find predict trajectory by average
-        # predict_trajectory = []
-        # for s in range(steps+1):
-        #     x_val = np.mean(total_trajectories[:,s,0])
-        #     y_val = np.mean(total_trajectories[:,s,1])
-        #     z_val = np.mean(total_trajectories[:,s,2])
-        #     predict_trajectory.append([x_val, y_val, z_val])
-        #     print(predict_trajectory)
-        #     return
-
-        # # Compute Variance
-        # variance = []
-        # for r in range(steps):
-        #     variance.append(self.get_min_sphere(predict_trajectory[r],rectangle[r][0], rectangle[r][1], rectangle[r][2], rectangle[r][3], rectangle[r][4], rectangle[r][5]))
-
-        # if visualize:
-        #     # Trajectory Plot
-        #     # fig = plt.figure(1)
-        #     # ax = fig.add_subplot(111, projection='3d')
-
-        #     # predict_trajectory = np.array(predict_trajectory)
-        #     # ax.plot(predict_trajectory[:,0], predict_trajectory[:,1], predict_trajectory[:,2], '-x',color='r', label="Avg Traj")
-
-        #     # for t in range(num_trajectory):
-        #     #     ax.plot(total_trajectories[t][:,0], total_trajectories[t][:,1], total_trajectories[t][:,2], '-x',color='b')
-
-        #     # for r in range(len(rectangle)):
-        #     #     self.plot_rec(ax, rectangle[r][0], rectangle[r][1], rectangle[r][2], rectangle[r][3], rectangle[r][4], rectangle[r][5])
-
-        #     # # plot_rec(ax, 0,1,-bound_y/2,bound_y/2,-bound_z/2,bound_y/2)
-        #     # ax.set_xlabel('X')
-        #     # ax.set_ylabel('Y')
-        #     # ax.set_zlabel("Z")
-        #     # plt.show()
-
-        #     # Variance Plot
-        #     s = np.arange(0,steps,1)
-        #     print(variance)
-        #     plt.figure(figsize=(10, 5))
-        #     plt.plot(s,variance, marker='o')
-        #     plt.xlabel('Steps')
-        #     plt.ylabel('Variance')
-        #     plt.title('Prediction Variance')
-        #     plt.grid(True)
-        #     plt.show()
-
-
-        return rectangle, total_trajectories
-    
 if __name__ == "__main__":
-    p = Prediction()
-    for i in range(1):
-        starttime = time.time()
-        rectangles, total_traj = p.find_prediction(torch.tensor([2.0,3.,4.,0.,0.,0.]), torch.tensor([1.8,0.,4.]), 0.01, 4, steps = 2, num_trajectory = 6, visualize = False)
-        print("runtime",time.time()-starttime)
-        print(rectangles)
-
-    # find bound 
-    fov_h = 70
-    aspect_ratio = 4/3
-    fov_h_radians = fov_h*np.pi/180.0
-    fov_v_radians = 2 * np.atan(np.tan(fov_h_radians / 2) / aspect_ratio)
-        
+    p = Prediction(num_trajectory = 100, steps=10)
     
-    def calculate_viewable_area(self, distance):
-        viewable_width = 2 * distance * torch.tan(self.fov_h_radians / 2)
-        viewable_height = 2 * distance * torch.tan(self.fov_v_radians / 2)
-        
-        return viewable_width, viewable_height
+    leader_state = np.array([2.0,0,4.,0.,0.,0.])
+    ego_pose = np.array([1.8,0.,4.])
+
+    starttime = time.time()
+
+    total_trajectories, backoff, bounds = p.compute_reach(leader_state, 0.1, 5, 0.94)
+
+    bound_vertex = np.array([bounds[0]-backoff, (bounds[2]+bounds[3])/2,  (bounds[4]+bounds[5])/2])
+    
+    print("runtime",time.time()-starttime)
+
+
+    ########## Visualize prep ########## 
+    np_total_trajectories = total_trajectories.cpu().numpy()
+    trajectories = []
+    for j in range(p.num_trajectory):
+        single_trajectory = np.zeros((3, p.steps+1))
+        index = 0
+        for s in range(0, np_total_trajectories.shape[1], 6):
+            single_trajectory[0,index] = np_total_trajectories[j][s]
+            single_trajectory[1,index] = np_total_trajectories[j][s+1]
+            single_trajectory[2,index] = np_total_trajectories[j][s+2]
+            index+=1
+        trajectories.append(single_trajectory)
+
+    # 3D Figure
+    fig = plt.figure(1)
+    ax = fig.add_subplot(111, projection='3d')
+
+    for t in range(p.num_trajectory):
+        ax.plot(trajectories[t][0,:], trajectories[t][1,:], trajectories[t][2,:], '-x',color='b')
+    
+    p.plot_rec(ax, bounds[0], bounds[1], bounds[2], bounds[3], bounds[4], bounds[5])
+
+    ax.scatter(leader_state[0],leader_state[1], leader_state[2], color='red', label='leader drone')
+    ax.scatter(ego_pose[0],ego_pose[1], ego_pose[2], color='green', label='chaser drone')
+    ax.scatter(bound_vertex[0], bound_vertex[1], bound_vertex[2], color='green', label='chaser drone')
+
+    # Plot pyramid
+    base_corners = np.array([
+    [bounds[0], bounds[2], bounds[4]],
+    [bounds[0], bounds[3], bounds[4]],
+    [bounds[0], bounds[3], bounds[5]],
+    [bounds[0], bounds[2], bounds[5]]
+    ])
+
+    vertex = bound_vertex
+    points = np.vstack((base_corners, vertex))
+
+    # Plot the base of the pyramid
+    for i in range(len(base_corners)):
+        next_i = (i + 1) % len(base_corners)
+        ax.plot([base_corners[i, 0], base_corners[next_i, 0]],
+                [base_corners[i, 1], base_corners[next_i, 1]],
+                [base_corners[i, 2], base_corners[next_i, 2]], color='b')
+
+    # Plot the edges connecting the base to the vertex
+    for i in range(len(base_corners)):
+        ax.plot([base_corners[i, 0], vertex[0]],
+                [base_corners[i, 1], vertex[1]],
+                [base_corners[i, 2], vertex[2]], color='b')
+
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel("Z")
+    ax.legend()
+    plt.show()
