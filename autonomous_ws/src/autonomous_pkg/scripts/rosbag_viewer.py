89c2659479e9b7853262e1015d2f97f203b2da04
Ben Yang <byang11259@hotmail.com>
2024-08-02 18:53:01 -0500

    cleaned up old scripts

--- autonomous_ws/src/autonomous_pkg/scripts/rosbag_viewer.py
+++
@@ -1,5 +0,0 @@
-import rosbag
-
-with rosbag.Bag('my_data.bag', 'r') as bag:
-    for topic, msg, t in bag.read_messages():
-        print(f"Topic: {topic}, Message: {msg}")
--- data_collection/lookpic.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-path = "./realsense_color.jpg"
-
-img = cv2.imread(path)
-cv2.imshow("img",img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
